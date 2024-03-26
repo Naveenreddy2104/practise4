d18afaaaa6380bdcb3ba15db670e6bc44153de4b
Naveenreddy2104 <90496227+Naveenreddy2104@users.noreply.github.com>
2024-03-26 21:39:23 +0530

    Add files via upload

---
+++ AuthorService.java
@@ -0,0 +1,23 @@
+// package com.example.demo.service;
+
+// import java.util.List;
+
+// import com.example.demo.entity.Author;
+
+// public interface AuthorService {
+    
+//     boolean insertAuthor(Author a);
+
+//     boolean insertAuthor(String title, Double price);
+
+//     List<Author> selectAuthor();
+
+//     Author selectAuthorById(Long id);
+
+//     boolean deleteAuthor(Long id);
+        
+//     Author updateAuthor(Long id, String title);
+
+//     Author updateAuthor(Long sno, String title, Double price);
+
+// }
